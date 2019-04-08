5c3094f53e0e7ec2478a6d64be0c22ff9eaa2a3d
Leon Niesler <leon.niesler@gmail.com>
2019-04-08 17:51:42 +0200

    added ports

--- website_flask/flaskr/__init__.py
+++ website_flask/flaskr/__init__.py
@@ -40,3 +40,8 @@ def create_app(test_config=None):
     app.add_url_rule('/', endpoint='index')
 
     return app
+
+
+if __name__ == "__main__":
+    app=create_app()
+    app.run(host="0.0.0.0", port=80)
